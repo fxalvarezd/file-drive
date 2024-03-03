869fb6f43b9814aeb582ca2aac1f5a9f51ca5b74
Francisco X. Alvarez <franciscoalvarezd@gmail.com>
2024-03-03 17:48:11 -0600

    adding schema and checking for identity in handlers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,13 @@ import {
   SignedOut,
   useSession,
 } from "@clerk/nextjs";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
 
 export default function Home() {
+  const files = useQuery(api.files.getFiles);
+  const createFile = useMutation(api.files.createFile);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <SignedOut>
@@ -23,6 +28,18 @@ export default function Home() {
           <Button>Sign Out</Button>
         </SignOutButton>
       </SignedIn>
+
+      {files?.map((file) => {
+        return <div key={file._id}>{file.name}</div>;
+      })}
+
+      <Button
+        onClick={() => {
+          createFile({ name: "hello FX" });
+        }}
+      >
+        Add entry
+      </Button>
     </main>
   );
 }
